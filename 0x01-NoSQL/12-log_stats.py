2483c2d87c97c8ab47faad9737b082f5d475e371
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-07-18 11:10:17 -0400

    Log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
-"""
-Where can I learn Python?
-"""
+""" Log Stats module. """
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    number = nginx_collection.count()
-    number_get = nginx_collection.find({"method": "GET"}).count()
-    number_post = nginx_collection.find({"method": "POST"}).count()
-    number_put = nginx_collection.find({"method": "PUT"}).count()
-    number_patch = nginx_collection.find({"method": "PATCH"}).count()
-    number_delete = nginx_collection.find({"method": "DELETE"}).count()
-    number_status = nginx_collection.find(
-        {"method": "GET", "path": "/status"}).count()
+client = MongoClient(host="localhost", port=27017)
+db = client.logs
 
-    print("{} logs".format(number))
+if __name__ == "__main__":
+    print("{} logs".format(db.nginx.count_documents({})))
     print("Methods:")
-    print("\tmethod GET: {}".format(number_get))
-    print("\tmethod POST: {}".format(number_post))
-    print("\tmethod PUT: {}".format(number_put))
-    print("\tmethod PATCH: {}".format(number_patch))
-    print("\tmethod DELETE: {}".format(number_delete))
-    print("{} status check".format(number_status))
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        print("\tmethod {}: {}".format(
+            method, db.nginx.count_documents({"method": method})))
+
+    print("{} status check".format(db.nginx.count_documents(
+        {"method": "GET", "path": "/status"})))
